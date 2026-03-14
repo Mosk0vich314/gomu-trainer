84cee72105d13d11f113c0896f65bab09a254fcb
Mosk0vich314 <157249906+Mosk0vich314@users.noreply.github.com>
2026-03-14 11:35:07 +0100

    Auto-deploy build v2026.03.14.1135

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gomu-trainer-v2026.03.14.1129'; // Increment this!
+const CACHE_NAME = 'gomu-trainer-v2026.03.14.1135'; // Increment this!
 const urlsToCache = [
   './',
   './index.html',
