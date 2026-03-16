2388e5e3282f7ca7e98a9b9499561f0c8f922889
Mosk0vich314 <157249906+Mosk0vich314@users.noreply.github.com>
2026-03-16 23:24:47 +0100

    Password rotation (v2026.03.16.2324)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gomu-trainer-v2026.03.16.2304'; // Increment this!
+const CACHE_NAME = 'gomu-trainer-v2026.03.16.2324'; // Increment this!
 const urlsToCache = [
   './',
   './index.html',
